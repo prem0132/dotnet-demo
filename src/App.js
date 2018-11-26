3fc13b2adda8cdc84ed5112b0e321c49d343f74d
prem0132 <prem.it.rits@gmail.com>
2018-11-26 15:40:20 +0530

    adding jenkinsfile

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-	    Namaste!!!
+	    Namaste!!!!
 	  </p>
           <a
             className="App-link"
